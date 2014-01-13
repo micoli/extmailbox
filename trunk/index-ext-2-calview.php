c5102888a13608de585339ae6fb0d9e600b39c2e
olivier@micoli.org <olivier@micoli.org@2adc55dd-04f1-3136-73ff-413b30bded63>
2014-01-13 02:10:22 +0000

    calendar widget

---
+++ trunk/index-ext-2-calview.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Ext 2 sandbox Calendar</title>
+		<link rel="stylesheet" type="text/css"	href="/ext-2.2.1/release/resources/css/ext-all.css" />
+		<script type="text/javascript"			src="/ext-2.2.1/release/adapter/ext/ext-base.js"></script>
+		<script type="text/javascript"			src="/ext-2.2.1/release/ext-all-debug.js"></script>
+		<link rel="stylesheet" type="text/css"	href="modules/calendarview/Ext.eu.sm.CalendarView.css" />
+		<script type="text/javascript"			src="modules/calendarview/Ext.eu.sm.CalendarView.js"></script>
+		<script type="text/javascript"			src="modules/calendarview/sandboxCalview.js"></script>
+		<script>
+			Ext.BLANK_IMAGE_URL = '/ext-2.2.1/release/resources/images/default/s.gif';
+		</script>
+		<!-- script type="text/javascript" src="sandbox.js"></script -->
+		<script type="text/javascript">console.log(new Date);</script>
+	</head>
+	<body>
+		<div id="mainPanel" style="width:1024x;"></div>
+	</body>
+</html>
