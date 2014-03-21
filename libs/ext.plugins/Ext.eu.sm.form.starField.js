38bb0ab1b2b848e7e84a81883946f7fe4108b1ce
olivier <olivier@micoli.org>
2014-03-21 00:17:00 +0000

    starField and htmlRenrederField

---
+++ libs/ext.plugins/Ext.eu.sm.form.starField.js
@@ -0,0 +1,20 @@
+Ext.ns('Ext.eu.sm.form');
+
+Ext.eu.sm.form.starField = Ext.extend(Ext.eu.sm.form.renderedField,{
+	starType	: 2 ,//[1,2,3]
+	mode		: 'proportional',//[proportional,byhalf]
+	renderer	: function (v){
+		v = parseFloat(v);
+		v = Math.min(v,5);
+		v = Math.max(v,0);
+		if(this.mode=='byhalf'){
+			v = parseInt(v*2)/2;
+		}
+		return '<div class="Ext_eu_sm_form_starField_'+this.starType+'_off">'+
+					'<div class="Ext_eu_sm_form_starField_'+this.starType+'_on" style="width:'+(v/5*100)+'%;">'+
+						'<img src="'+Ext.BLANK_IMAGE_URL+'">'+
+					'</div>'+
+				'</div>';
+	}
+});
+Ext.reg('eu.sm.form.starField',Ext.eu.sm.form.starField);
