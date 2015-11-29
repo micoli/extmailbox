a1380779d51c6cd0aeae57e92ef66ff0be02b512
o.michaud <olivier@micoli.org>
2015-11-29 10:27:01 +0100

    composer fix

--- modules/mailbox/vendor/composer/autoload_real.php
+++ modules/mailbox/vendor/composer/autoload_real.php
@@ -40,6 +40,20 @@ class ComposerAutoloaderInit9bd392105653427bd30003141476f3c4
 
         $loader->register(true);
 
+        $includeFiles = require __DIR__ . '/autoload_files.php';
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire9bd392105653427bd30003141476f3c4($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire9bd392105653427bd30003141476f3c4($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
